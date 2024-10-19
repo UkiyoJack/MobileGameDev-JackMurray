0576e2c7d92ee44022de3a8f0f8683ddfb4e6bc8
UkiyoJack <jack.mry1@gmail.com>
2024-10-19 02:11:00 +0100

    Android and Windows build, edited project settings

--- mobile game/Assets/Score.cs
+++ mobile game/Assets/Score.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
---
+++ mobile game/README.txt
@@ -0,0 +1,10 @@
+JACK MURRAY
+S22123300
+COMPUTER GAMES TECHNOLOGY Y3
+
+ROOT:
+	Assets - all project assets
+
+	BUILDS - Both android .apk and Windows .exe build files
+	
+	
